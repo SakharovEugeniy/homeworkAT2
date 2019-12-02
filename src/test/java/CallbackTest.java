0ee580448661f430b9b2e9ca4040cfec44c6d7c1
Sakharov Eugeniy <Worgwhd@mail.ru>
2019-12-02 23:35:07 +0600

    02.12.19 dont't work css and java -jar app-order/jar

--- src/test/java/CallbackTest.java
+++ src/test/java/CallbackTest.java
@@ -9,12 +9,20 @@ public class CallbackTest {
     @Test
     void shouldSubmitRequest(){
         open("http://localhost:9999");
-        SelenideElement form = $( "[data-test-id=callback-form]");
-        form.$("[data-test-id=name]input").setValue("Василий");
-        form.$("[data-test-id=phone]input").setValue("+79270000000");
-        form.$("[data-test-id=agreement]").click();
-        form.$("[data-test-id=submit]").click();
-        $(".alert-success").shouldHave(exactText("Ваша заявка успешно отправлена!"));
+        $(input[type='text']).setValue("Василий");
+
+
+
+
+
+
+//        SelenideElement form = $( "[input class=input_contorl]");
+//        form.$("[input class=input_contorl").setValue("Василий");
+//        form.$("[data-test-id=phone]input").setValue("+79270000000");
+//        form.$("[data-test-id=agreement]").click();
+//        form.$("[data-test-id=submit]").click();
+//        $(".alert-success").shouldHave(exactText("Ваша заявка успешно отправлена!"));
+
 
 
     }
