1a51843534333ab8481962d2773571e42b845e73
Sakharov Eugeniy <Worgwhd@mail.ru>
2020-01-16 22:43:09 +0600

    1st version css

--- src/test/java/CallbackTest.java
+++ src/test/java/CallbackTest.java
@@ -9,7 +9,11 @@ public class CallbackTest {
     @Test
     void shouldSubmitRequest(){
         open("http://localhost:9999");
-        $(input[type='text']).setValue("Василий");
+        $("input[type='text']").setValue("Василий");
+        $("input[type='tel']").setValue("+79270000000");
+        $("[data-test-id=agreement]").click();
+        $("[data-test-id=span.button__text]").click();
+        $("p.paragraph").shouldHave(exactText("Ваша заявка успешно отправлена!"));
 
 
 
