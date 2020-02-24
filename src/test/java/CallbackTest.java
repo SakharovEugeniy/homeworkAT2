9d163a75f46499ca050be58921f470adbddfa1f5
Sakharov Eugeniy <Worgwhd@mail.ru>
2020-02-24 23:17:08 +0600

    1st working test

--- src/test/java/CallbackTest.java
+++ src/test/java/CallbackTest.java
@@ -5,29 +5,19 @@ import static com.codeborne.selenide.Condition.exactText;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
 
-public class CallbackTest {
+class CallbackTest {
     @Test
-    void shouldSubmitRequest(){
+    void shouldSubmitRequest() throws InterruptedException {
         open("http://localhost:9999");
         $("input[type='text']").setValue("Василий");
+//        Thread.sleep(10000);
         $("input[type='tel']").setValue("+79270000000");
+//        Thread.sleep(10000);
         $("[data-test-id=agreement]").click();
-        $("[data-test-id=span.button__text]").click();
-        $("p.paragraph").shouldHave(exactText("Ваша заявка успешно отправлена!"));
-
-
-
-
-
-
-//        SelenideElement form = $( "[input class=input_contorl]");
-//        form.$("[input class=input_contorl").setValue("Василий");
-//        form.$("[data-test-id=phone]input").setValue("+79270000000");
-//        form.$("[data-test-id=agreement]").click();
-//        form.$("[data-test-id=submit]").click();
-//        $(".alert-success").shouldHave(exactText("Ваша заявка успешно отправлена!"));
-
-
-
+//        Thread.sleep(10000);
+        $("button[type='button']").click();
+//        Thread.sleep(10000);
+        $("[data-test-id=order-success]").shouldHave(exactText("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время."));
+//        Thread.sleep(10000);
     }
 }
