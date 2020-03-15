6e4607659f891f55ff262c3ebfb0f83a41ed9e20
Sakharov Eugeniy <Worgwhd@mail.ru>
2020-03-15 22:47:25 +0600

    Changing permission of gradlew

--- src/test/java/CallbackTest.java
+++ src/test/java/CallbackTest.java
@@ -10,14 +10,9 @@ class CallbackTest {
     void shouldSubmitRequest() throws InterruptedException {
         open("http://localhost:9999");
         $("input[type='text']").setValue("Василий");
-//        Thread.sleep(10000);
         $("input[type='tel']").setValue("+79270000000");
-//        Thread.sleep(10000);
         $("[data-test-id=agreement]").click();
-//        Thread.sleep(10000);
         $("button[type='button']").click();
-//        Thread.sleep(10000);
         $("[data-test-id=order-success]").shouldHave(exactText("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время."));
-//        Thread.sleep(10000);
     }
 }
